a350a6b765d8768c62d1a4ecb6b0bc02916ee308
ikchan-ju <couquedasom@gmail.com>
2022-06-07 09:32:00 +0900

    duplicate mechanism

--- Assets/FireBall.cs
+++ Assets/FireBall.cs
@@ -9,6 +9,8 @@ public class FireBall : MonoBehaviour
     public float angle=0;
     public float speed;
     public float trigger;
+    public int loopCount;
+    private int count = 0;
     private Quaternion rotation;
     // Start is called before the first frame update
     void Start()
@@ -21,15 +23,25 @@ public class FireBall : MonoBehaviour
     void Update()
     {
         mana += manaChargingSpeed * Time.deltaTime;
+        angle -= mana*Time.deltaTime*180;
+        angle = angle%360;
+        transform.Rotate(0, 0, -mana*Time.deltaTime*180);
+        if(count < loopCount){
+            if(mana >= trigger){
+                print("Add Fire Ball");
+                mana = 0;
+                count++;
+                AddMagic();
+            }
+            return;
+        }
         if(mana >= trigger){
             print("Shooting Fire Ball");
             transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0, 0);
         }
-        // else
-        {
-            angle -= mana*Time.deltaTime*180;
-            angle = angle%360;
-            transform.Rotate(0, 0, -mana*Time.deltaTime*180);
-        }
+    }
+
+    void AddMagic(){
+
     }
 }
